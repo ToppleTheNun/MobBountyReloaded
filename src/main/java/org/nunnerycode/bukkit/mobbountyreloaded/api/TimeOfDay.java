cee132233baf33344415f365ee712efe3632d3e6
Richard Harrah <topplethenunnery@gmail.com>
2014-03-07 23:13:10 -0500

    adding TimeOfDay enum

---
+++ src/main/java/org/nunnerycode/bukkit/mobbountyreloaded/api/TimeOfDay.java
@@ -0,0 +1,59 @@
+package org.nunnerycode.bukkit.mobbountyreloaded.api;
+
+public enum TimeOfDay {
+
+  DAY("Day", 0, 10000), SUNSET("Sunset", 10000, 1500), NIGHT("Night", 11500, 7000),
+  SUNRISE("Sunrise", 18500, 1500);
+
+  private final String name;
+  private final int starts;
+  private final int length;
+
+  private TimeOfDay(String name, int starts, int length) {
+    this.name = name;
+    this.starts = starts;
+    this.length = length;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getStarts() {
+    return starts;
+  }
+
+  public int getLength() {
+    return length;
+  }
+
+  public static TimeOfDay getTimeFromString(String time) {
+    if (time.equalsIgnoreCase("day")) {
+      return DAY;
+    }
+    if (time.equalsIgnoreCase("sunset")) {
+      return SUNSET;
+    }
+    if (time.equalsIgnoreCase("night")) {
+      return NIGHT;
+    }
+    if (time.equalsIgnoreCase("sunrise")) {
+      return SUNRISE;
+    }
+    return null;
+  }
+
+  public static TimeOfDay getTimeOfDay(long time) {
+    if (time >= 0 && time < 10000) {
+      return DAY;
+    }
+    if (time >= 10000 && time < 11500) {
+      return SUNSET;
+    }
+    if (time >= 11500 && time < 18500) {
+      return NIGHT;
+    }
+    return SUNRISE;
+  }
+
+}
