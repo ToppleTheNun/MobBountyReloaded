cef975ecf6fa2f1c6d8ad1c6ea7c8d8ae8144a86
Richard Harrah <topplethenunnery@gmail.com>
2014-04-07 12:49:37 -0400

    adding GroupHandlers

---
+++ src/main/java/org/nunnerycode/bukkit/mobbountyreloaded/api/groups/IGroupHandler.java
@@ -0,0 +1,8 @@
+package org.nunnerycode.bukkit.mobbountyreloaded.api.groups;
+
+import org.bukkit.entity.Player;
+
+public interface IGroupHandler {
+
+  String getGroup(Player player);
+}
---
+++ src/main/java/org/nunnerycode/bukkit/mobbountyreloaded/groups/GroupHandler.java
@@ -0,0 +1,21 @@
+package org.nunnerycode.bukkit.mobbountyreloaded.groups;
+
+import net.milkbowl.vault.permission.Permission;
+
+import org.bukkit.entity.Player;
+import org.nunnerycode.bukkit.mobbountyreloaded.api.groups.IGroupHandler;
+
+public final class GroupHandler implements IGroupHandler {
+
+  private Permission permissions;
+
+  private GroupHandler(Permission permissions) {
+    this.permissions = permissions;
+  }
+
+  @Override
+  public String getGroup(Player player) {
+    return permissions.getPrimaryGroup(player);
+  }
+
+}
